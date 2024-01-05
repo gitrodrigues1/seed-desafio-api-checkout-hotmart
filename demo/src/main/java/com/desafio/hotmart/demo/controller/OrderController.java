d3cdbfb89103bd007395243d96000bb425f69317
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-01-05 15:44:09 -0300

    Add OrderController class

---
+++ demo/src/main/java/com/desafio/hotmart/demo/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.desafio.hotmart.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.hotmart.demo.model.SalesOrder;
+import com.desafio.hotmart.demo.records.CreateSalesOrder;
+import com.desafio.hotmart.demo.service.SalesOrderService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("api/v1/orders")
+public class OrderController {
+
+    private SalesOrderService orderService;
+
+    public OrderController(SalesOrderService orderService) {
+        this.orderService = orderService;
+    }
+    
+    @PostMapping()
+    public SalesOrder createOrder(@RequestBody CreateSalesOrder form) {
+        return orderService.createOrder(form);
+    }
+
+    @GetMapping
+    public List<SalesOrder> getAllOrders() {
+        return orderService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public SalesOrder getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+    
+}
